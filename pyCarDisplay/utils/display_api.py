74612e0c4120f7eac3c77b1a70263783d68007c0
Ryan Barron <ryanb4@umbc.edu>
2021-03-15 23:17:08 -0400

    reoriented the display file to API data prefernece

--- pyCarDisplay/utils/display_api.py
+++ pyCarDisplay/utils/display_api.py
@@ -2,39 +2,44 @@ import PySimpleGUI as sg
 import os
 
 class Display():
-	def __init__(o, img_data_path, detected_images):
+
+	# need to pass list of
+	def __init__(o, image_frames_dict):
 		o.close = sg.WIN_CLOSED
+
 		o.IMG_PATH = img_data_path
 		o.DET_IMG_PATH = detected_images
-		o.speed = 20
-		o.n_images = 0
+		o.speed = 0
 		o.files = []
 
-		#Just for demo
-		o.wolf = o.DET_IMG_PATH+"wolf.png"
+		o.image_frames_dict = image_frames_dict
+		o.n_images = len(image_frames_dict)
 
 	def img(o, path, key):
 		return sg.Image(path, key=key)
 
 	def define_layout(o):
 		return [
+            [sg.Button("Play"), sg.Button("Pause")],
+            [sg.ProgressBar(o.n_images, orientation='h', size=(50, 5), key='progressbar')],
 			[sg.Text("Frame: 1", size=(50, 1), key="frame")],
-			[sg.Text("True Speed: "+str(o.speed), key="speed"), sg.Text("-- Kalman speed: "+str(o.speed), key="kspeed")],
-			[o.img(o.wolf, "IMG")],
+			[sg.Text("True Speed: " + str(o.speed), key="speed"), sg.Text("-- Kalman speed: " + str(o.speed), key="kspeed")],
+			[o.img("", "IMG")],
 			[
-				o.img(o.wolf, "IMG1"),
-				o.img(o.wolf, "IMG2"),
-				o.img(o.wolf, "IMG3"),
-				o.img(o.wolf, "IMG4"),
-				o.img(o.wolf, "IMG5")
-			],
-			[sg.ProgressBar(o.n_images, orientation='h', size=(50, 5), key='progressbar')],
-			[sg.Button("Play"), sg.Button("Pause")]
+				o.img("", "IMG1"),
+				o.img("", "IMG2"),
+				o.img("", "IMG3"),
+				o.img("", "IMG4"),
+				o.img("", "IMG5")
+			]
 		]
 
 	def play(o):
-		o.files = os.listdir(o.IMG_PATH)
-		o.n_images = len(o.files)
+		#o.files = os.listdir(o.IMG_PATH)
+		image_frames_dict = {
+		'0': {'image':'path/to/image', 'detected_object_images': ['path/to/detected/image1', 'path/to/detected/image1'] },
+		'1': {'image':'path/to/image', 'detected_object_images': ['path/to/detected/image1'] }
+		}
 
 		# Create the window
 		window = sg.Window("Autonomous Vehicle Object & Distance Detection", o.define_layout())
@@ -61,20 +66,16 @@ class Display():
 
 			# Default is to play the images
 		    else:
-		        window.FindElement("IMG").Update(o.IMG_PATH + "\\" + o.files[i % o.n_images])
 
-				# This is just for demonstation purposes
-		        if i == (o.n_images*3) // 4:
-		            window.FindElement("IMG2").Update("")
-		            window.FindElement("IMG3").Update("")
-		            window.FindElement("IMG4").Update("")
-		            window.FindElement("IMG5").Update("")
+				# update main display
+		        window.FindElement("IMG").Update(o.image_frames_dict[str(i)][image])
 
-				# Also just for demonstation purposes
-		        elif i == o.n_images // 4:
-		            window.FindElement("IMG2").Update(o.DET_IMG_PATH + "wolf.png")
-		            window.FindElement("IMG3").Update(o.DET_IMG_PATH + "leaf.png")
-
+				# update up to 5 of the images of detected objects
+				num = 0
+				for detected_image in o.image_frames_dict[str(i)]["detected_object_images"]:
+					if num < 5:
+			            window.FindElement("IMG"+str(num)).Update(detected_image)
+						num += 1
 
 		        window.FindElement("frame").Update("Frame: "+str(i + 1))
 
