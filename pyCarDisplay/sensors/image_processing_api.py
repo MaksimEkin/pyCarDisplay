eaa0ade3057e5eb4210075c294e9170d7641e524
Ryan <ryanb4@umbc.edu>
2021-04-03 16:00:16 -0400

    Started adding image processing functionality -- take_picture and synch_objDet_depDet_data

--- pyCarDisplay/sensors/image_processing_api.py
+++ pyCarDisplay/sensors/image_processing_api.py
@@ -1,9 +1,23 @@
 """
 """
+from PIL import Image
+
 class ImageProcessing():
-    def __init__(self,
-                 car_images_path:str):
-        pass
-    
-    def process_images_path(self):
-        return ""
+    def __init__(self, car_images_path:str):
+        self.img_path = car_images_path
+        self.img = None
+
+    def take_picture(self):
+        return Image.open(self.img_path, mode='r').convert('RGB')
+
+    def synch_objDet_depDet_data(box_locations:list, depth_image):
+        """Takes in object locations in a snapshot and the depth image to return depth heatmap of the objects.
+
+        Args:
+            param1: list of the object locations found in object detection machine learning model.
+            param2: image returned from depth detection machine learning model.
+
+        Returns:
+            list of cropped images at the location of the object detection
+            and the depth rendering of the depth detection model.
+        """
