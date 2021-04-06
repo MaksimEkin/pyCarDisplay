ee69641b311f17c3db85b82e60611c51f96fe59e
MaksimEkin <meren1@umbc.edu>
2021-04-06 01:54:10 -0400

    changes.

--- pyCarDisplay/utils/display_api.py
+++ pyCarDisplay/utils/display_api.py
@@ -3,26 +3,17 @@ import pandas as pd
 from PIL import Image
 import io
 from io import StringIO
-from tabulate import tabulate
 
 class Display():
 
     # need to pass a frame dictionary that contains dictionaries of image paths and detected image lists
-<<<<<<< HEAD
+
     def __init__(self, speed: int, total_frames: int):
         self.cropped_img_displayed = 5
         self.close = sg.WIN_CLOSED
         self.speed = speed
         self.total_frames = total_frames
-
-=======
-    def __init__(o, speed: int, total_frames: int):
-        o.cropped_img_displayed = 5
-        o.close = sg.WIN_CLOSED
-        o.speed = speed
-        o.total_frames = total_frames
-        o.verbose = False
->>>>>>> ba0a8dc4e080f0daed0d03dcbb3e6f0fe71ef578
+        self.verbose = False
         # Create the window
         self.window = sg.Window("Autonomous Vehicle Object & Distance Detection", self.define_layout())
         self.progress_bar = self.window['progressbar']
@@ -38,54 +29,25 @@ class Display():
 
     def depth_images_update(self, cropped_depth_images):
         for num, detected_image in enumerate(cropped_depth_images):
-<<<<<<< HEAD
+
             if num < self.cropped_img_displayed:
                 self.update_window("IMG" + str(num + 1), self.format_pil_img(detected_image), detected_image.size)
-=======
-            if num < o.cropped_img_displayed:
-                o.update_window("IMG" + str(num + 1), o.format_pil_img(detected_image), detected_image.size)
 
-
-    def speed_update(o, imu_data, kalman_imu_data, imu_name):
-        
-        if isinstance(imu_data, pd.DataFrame):
-            display_imu_data = tabulate(imu_data, headers='keys', tablefmt='psql')
-        else:
-            display_imu_data = tabulate(pd.DataFrame({str(imu_name):imu_data}), headers='keys', tablefmt='psql')
-        
-        if o.verbose:
-<<<<<<< HEAD
-            print("Examine imu=", display_imu_data)
-            print("Examine Kalman=", kalman_imu_data['data'])
-
-        gold_speed = display_imu_data
-=======
-            print("Examine imu=", imu_data['data'][0])
-            print("Examine Kalman=", kalman_imu_data['data'][0])
->>>>>>> ba0a8dc4e080f0daed0d03dcbb3e6f0fe71ef578
 
     def speed_update(self, imu_data, kalman_imu_data):
-        gold_speed = "True Speed: " + str(imu_data['data'][0])
-<<<<<<< HEAD
+        if self.verbose:
+            print("Examine imu=", imu_data['data'])
+            print("Examine Kalman=", kalman_imu_data['data'])
+
+    def speed_update(self, imu_data, kalman_imu_data):
+        gold_speed = "True Speed: " + str(imu_data['data'])
+
+        if self.verbose:
+            print("gold_speed", gold_speed)
         self.window.FindElement("speed").Update(gold_speed)
-=======
->>>>>>> 09d01177205150000a35d3e9fbb06c2b0d1c6c0e
-        
-        o.window.FindElement("speed").Update(gold_speed)
->>>>>>> ba0a8dc4e080f0daed0d03dcbb3e6f0fe71ef578
 
-<<<<<<< HEAD
-        kalman_speed = "Kalman speed: " + str(kalman_imu_data['data'])
-        o.update_window("kspeed", kalman_speed)
-
-
-    def reset_depth_images(o, cropped_depth_images):
-        for num in range(o.cropped_img_displayed):
-            o.update_window("IMG" + str(num + 1), "")
-=======
         kalman_speed = "Kalman speed: " + str(kalman_imu_data['data'][0])
         self.update_window("kspeed", kalman_speed)
->>>>>>> 09d01177205150000a35d3e9fbb06c2b0d1c6c0e
 
     def reset_depth_images(self, cropped_depth_images):
         for num in range(self.cropped_img_displayed):
@@ -104,25 +66,22 @@ class Display():
                 self.img("", "IMG3"),
                 self.img("", "IMG4"),
                 self.img("", "IMG5")
-            ]
+            ],
+
+           # headings = ['HEADER 1', 'HEADER 2', 'HEADER 3','HEADER 4']
         ]
 
     def end(self):
         self.window.close()
 
-<<<<<<< HEAD
     def play(self, annotated_image: Image, cropped_depth_images: list, imu_data: pd.DataFrame,
-             kalman_imu_data: pd.DataFrame, frame: int):
-=======
-    def play(o, annotated_image: Image, cropped_depth_images: list, imu_data: pd.DataFrame,
-             kalman_imu_data: pd.DataFrame, frame: int, verbose:bool, imu_name:str):
-        
-        
->>>>>>> ba0a8dc4e080f0daed0d03dcbb3e6f0fe71ef578
+             kalman_imu_data: pd.DataFrame, frame: int, verbose:bool):
+
+
         cropped_depth_images = ['heh', 'ehh']
-        
-        o.verbose = verbose
-        if o.verbose:
+
+        self.verbose = verbose
+        if self.verbose:
             print(type(annotated_image))
 
         # check if pause or play were clicked or if window closed
@@ -134,7 +93,7 @@ class Display():
 
         # update main display_api with detected objects
         #annotated_image.show()
-        with iself.BytesIO() as output:
+        with io.BytesIO() as output:
             annotated_image.save(output, format="PNG")
             contents = output.getvalue()
         self.update_window("IMG", contents, annotated_image.size)
@@ -148,10 +107,5 @@ class Display():
         self.update_window("frame", "Frame: " + str(frame))
 
         # current Speed and Kalman speed updated with api data
-<<<<<<< HEAD
-        o.speed_update(imu_data, kalman_imu_data, imu_name)
-        o.progress_bar.UpdateBar(frame + 1)
-=======
         self.speed_update(imu_data, kalman_imu_data)
         self.progress_bar.UpdateBar(frame + 1)
->>>>>>> 09d01177205150000a35d3e9fbb06c2b0d1c6c0e
