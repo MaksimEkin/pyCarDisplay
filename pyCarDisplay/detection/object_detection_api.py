55d3dde20f318da905aedb43529198477da8092a
MaksimEkin <meren1@umbc.edu>
2021-04-12 21:53:57 -0400

    added docstring.

--- pyCarDisplay/detection/object_detection_api.py
+++ pyCarDisplay/detection/object_detection_api.py
@@ -1,6 +1,12 @@
 """
+This is the object detection module.
 
-TODO CITE
+This code is borrowed from sgrvinod's GitHub repository named
+a-PyTorch-Tutorial-to-Object-Detection, and modified to work with pyCarDisplay.
+It preserved the main functionality of the unmodified version.
+
+Reference: https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Object-Detection
+
 """
 from torchvision import transforms
 from .__utils_obj_detection import *
@@ -9,32 +15,33 @@ import sys
 from termcolor import colored
 import os
 
+
 class ObjectDetection():
     def __init__(self,
-                 model_path:str,
-                 verbose:bool,
+                 model_path: str,
+                 verbose: bool,
                  img_resize_size=(300, 300),
                  norm_mean=[0.485, 0.456, 0.406],
                  norm_std=[0.229, 0.224, 0.225],
                  device="cpu"
                  ):
         """
-
+        Initilize the object detection class.
 
         Parameters
         ----------
         model_path : str
-            DESCRIPTION.
+            path to the pre-trained PyTorch model.
         verbose : bool
-            DESCRIPTION.
-        img_resize_size : TYPE, optional
-            DESCRIPTION. The default is (300, 300).
-        norm_mean : TYPE, optional
-            DESCRIPTION. The default is [0.485, 0.456, 0.406].
-        norm_std : TYPE, optional
-            DESCRIPTION. The default is [0.229, 0.224, 0.225].
-         : TYPE
-            DESCRIPTION.
+            Verbosity flag.
+        img_resize_size : tuple, optional
+            Image size during prediction. The default is (300, 300).
+        norm_mean : list, optional
+            model hyper-parameter. The default is [0.485, 0.456, 0.406].
+        norm_std : list, optional
+            model hyper-parameter. The default is [0.229, 0.224, 0.225].
+        device : str, optional
+            If 'gpu', model uses a CUDA device. The default is "cpu".
 
         Returns
         -------
@@ -44,7 +51,7 @@ class ObjectDetection():
         sys.path.append(os.system("pwd"))
         self.verbose = verbose
         self.model_path = model_path
-            
+
         # Use GPU if available
         if device == "cpu":
             self.device = torch.device("cpu")
@@ -53,25 +60,25 @@ class ObjectDetection():
                 self.device = torch.device("cuda")
             else:
                 sys.exit("No cuda device found!")
-        
+
         if self.verbose:
             print(colored("Object detection is using: " + str(self.device), "yellow"))
-        
 
         # Load model checkpoint
         if self.verbose:
-            print(colored("Object detection API is loading the model: " + str(self.model_path), "blue"))
-            
+            print(colored("Object detection API is loading the model: " +
+                          str(self.model_path), "blue"))
+
         checkpoint = torch.load(self.model_path, map_location=self.device)
         start_epoch = checkpoint['epoch'] + 1
 
         if self.verbose:
-            print(colored('Loaded checkpoint from epoch %d.\n' % start_epoch, "yellow"))
+            print(colored('Loaded checkpoint from epoch %d.\n' %
+                          start_epoch, "yellow"))
 
         self.model = checkpoint['model']
         self.model = self.model.to(device)
         self.model.eval()
-
 
         # Transforms
         self.resize = transforms.Resize(img_resize_size)
@@ -80,47 +87,63 @@ class ObjectDetection():
                                               std=norm_std)
 
     def detect(self,
-               original_image:Image,
+               original_image: Image,
                suppress=None,
                min_score=0.2,
                max_overlap=0.5,
                top_k=200):
         """
-
+        Predicts the objects in the image using the pre-trained model.
 
         Parameters
         ----------
-        original_image : str
-            DESCRIPTION.
-        suppress : list
-            DESCRIPTION.
-        min_score : TYPE, optional
-            DESCRIPTION. The default is 0.2.
-        max_overlap : TYPE, optional
-            DESCRIPTION. The default is 0.5.
-        top_k : TYPE, optional
-            DESCRIPTION. The default is 200.
+        original_image : Image
+            PIL image to be used for object detection.
+        suppress : TYPE, optional
+            DESCRIPTION. The default is None.
+        min_score : float, optional
+            min threshold for object to be classified. The default is 0.2.
+        max_overlap : float, optional
+            min threshold for object to be classified. The default is 0.5.
+        top_k : int, optional
+            how many objects to attempt to classify. The default is 200.
 
         Returns
         -------
-        TYPE
-            DESCRIPTION.
+        dict
+            Returns a dictionary in format: {"annotated_image": annotated_image, "box_info": box_info, "detected": True}
+            box_info is in format: box_info = {"text_size": text_sizes,"box_location": box_locations}, where the values
+            are list of coordinates for the detection boxes.
 
         """
 
-        return self.__detect_helper(original_image, min_score=min_score, max_overlap=max_overlap, top_k=top_k, suppress=suppress) # .show()
-
+        # .show()
+        return self.__detect_helper(original_image, min_score=min_score, max_overlap=max_overlap, top_k=top_k, suppress=suppress)
 
     def __detect_helper(self, original_image, min_score, max_overlap, top_k, suppress):
         """
-        Detect objects in an image with a trained SSD300, and visualize the results.
+        Performs the prediction.
 
-        :param original_image: image, a PIL Image
-        :param min_score: minimum threshold for a detected box to be considered a match for a certain class
-        :param max_overlap: maximum overlap two boxes can have so that the one with the lower score is not suppressed via Non-Maximum Suppression (NMS)
-        :param top_k: if there are a lot of resulting detection across all classes, keep only the top 'k'
-        :param suppress: classes that you know for sure cannot be in the image or you do not want in the image, a list
-        :return: {"annotated_image":annotated_image, "box_info":box_info}, annotated_image is a PIL Image
+        Parameters
+        ----------
+        original_image : PIL.Image
+            Kitti dataset 1 frame image.
+        min_score : float
+            detection threshold.
+        max_overlap : float
+            detection threshold.
+        top_k : int
+            number of objects to search.
+        suppress : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        dict
+            Returns a dictionary in format: {"annotated_image": annotated_image, "box_info": box_info, "detected": True}
+            box_info is in format: box_info = {"text_size": text_sizes,"box_location": box_locations}, where the values
+            are list of coordinates for the detection boxes.
+
         """
 
         # Transform
@@ -148,21 +171,22 @@ class ObjectDetection():
              original_image.height,
              original_image.width,
              original_image.height]
-            ).unsqueeze(0)
+        ).unsqueeze(0)
 
         det_boxes = det_boxes * original_dims
 
         # Decode class integer labels
-        det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
+        det_labels = [rev_label_map[l]
+                      for l in det_labels[0].to('cpu').tolist()]
 
         # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
         if det_labels == ['background']:
             # Just return original image
             box_info = {
-            "text_size":[],
-            "box_location":[]
+                "text_size": [],
+                "box_location": []
             }
-            return {"annotated_image":original_image, "box_info":box_info, "detected":False}
+            return {"annotated_image": original_image, "box_info": box_info, "detected": False}
 
         # Annotate
         annotated_image = original_image
@@ -179,7 +203,8 @@ class ObjectDetection():
 
             # Boxes
             box_location = det_boxes[i].tolist()
-            draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
+            draw.rectangle(xy=box_location,
+                           outline=label_color_map[det_labels[i]])
             draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
                 det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
             # draw.rectangle(xy=[l + 2. for l in box_location], outline=label_color_map[
@@ -189,11 +214,13 @@ class ObjectDetection():
 
             # Text
             text_size = font.getsize(det_labels[i].upper())
-            text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
+            text_location = [box_location[0] + 2.,
+                             box_location[1] - text_size[1]]
             textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
                                 box_location[1]]
 
-            draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
+            draw.rectangle(xy=textbox_location,
+                           fill=label_color_map[det_labels[i]])
             draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
                       font=font)
 
@@ -205,9 +232,8 @@ class ObjectDetection():
         # the box information
         box_info = dict()
         box_info = {
-            "text_size":text_sizes,
-            "box_location":box_locations
-            }
+            "text_size": text_sizes,
+            "box_location": box_locations
+        }
 
-        return {"annotated_image":annotated_image, "box_info":box_info, "detected":True}
-
+        return {"annotated_image": annotated_image, "box_info": box_info, "detected": True}
--- pyCarDisplay/sensors/imu_api.py
+++ pyCarDisplay/sensors/imu_api.py
@@ -1,14 +1,37 @@
 """
-
+This file simulates an IMU sensor. Data must be loaded from kitti_data_loader_api.py
+and passed to this module.
 """
 import numpy as np
 import pandas as pd
 import warnings
 import sys
 
+
 class IMU:
     def __init__(self, data, verbose=True, R_covariance=0.1, random_state=42):
-        assert type(data) is pd.DataFrame, "data should be type Pandas DataFrame"
+        """
+        Initilie the IMU sensor.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Pandas DataFrame of IMU data.
+        verbose : bool, optional
+            Verbosity flag. The default is True.
+        R_covariance : float, optional
+            Covariance for the random error. The default is 0.1.
+        random_state : bool, optional
+            random seed. The default is 42.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        assert type(
+            data) is pd.DataFrame, "data should be type Pandas DataFrame"
         self.imu_data = data
         self.num_frames = len(self.imu_data.frame.unique())
         self.params_avail = list(self.imu_data.columns.values)
@@ -20,52 +43,143 @@ class IMU:
             print("Initilized IMU")
             print("Current frame:" + str(self.frame))
             print("Total number of frames:" + str(len(self.imu_data)))
-            
+
     def get_R(self):
+        """
+        Returns the error covariance.
+
+        Returns
+        -------
+        float
+            Error covariance.
+
+        """
         return self.R_covariance
-            
+
     def get_avail_data(self):
-        return list(self.imu_data.columns) 
-        
+        """
+        Returns the available IMU features, i.e. column names.
+
+        Returns
+        -------
+        list
+            names of the IMU columns.
+
+        """
+        return list(self.imu_data.columns)
+
     def get_params(self):
+        """
+        Returns the object variables.
+
+        Returns
+        -------
+        dict
+            self variables.
+
+        """
         return vars(self)
-    
+
     def read_sensor(self, name=None, add_noise=True, advance_frame=True):
-        
+        """
+        Simulates reading one IMU sensor data and increments the frame.
+
+        Parameters
+        ----------
+        name : str, optional
+            Specify to read specific IMU data. The default is None.
+        add_noise : bool, optional
+            If True, error is added to the data. The default is True.
+        advance_frame : bool, optional
+            If True, frame is advanced to the next one. The default is True.
+
+        Returns
+        -------
+        dict
+            {"data": data, "noise": noise, "true": data-noise}.
+
+        """
+
         if self.frame > self.num_frames:
             if self.verbose:
-                warnings.warn("Reached at the end of available number of frames. Reverting to frame 0. Number of frames: " + str(self.num_frames))
+                warnings.warn(
+                    "Reached at the end of available number of frames. Reverting to frame 0. Number of frames: " + str(self.num_frames))
             self.frame = 0
-        
+
         noise = 0
-        
+
         data = None
         if name is None:
-            
+
             if add_noise:
-                num_columns = len(self.params_avail) - 1 # -1 for the frame column
+                num_columns = len(self.params_avail) - \
+                    1  # -1 for the frame column
                 noise = np.random.normal(0, self.R_covariance, num_columns)
-                data = self.imu_data[self.imu_data.frame == self.frame].loc[:, self.imu_data.columns != "frame"] + noise
+                data = self.imu_data[self.imu_data.frame ==
+                                     self.frame].loc[:, self.imu_data.columns != "frame"] + noise
             else:
-                data = self.imu_data[self.imu_data.frame == self.frame].loc[:, self.imu_data.columns != "frame"]
+                data = self.imu_data[self.imu_data.frame ==
+                                     self.frame].loc[:, self.imu_data.columns != "frame"]
         else:
             self.__check_param(name)
             if add_noise:
                 noise = np.random.normal(0, self.R_covariance, 1)
-            data =  self.imu_data[self.imu_data.frame == self.frame][str(name)].values + noise
-        
+            data = self.imu_data[self.imu_data.frame ==
+                                 self.frame][str(name)].values + noise
+
         if advance_frame:
             self.frame += 1
         return {"data": data, "noise": noise, "true": data-noise}
-        
-    def get_column(self, name:str):
+
+    def get_column(self, name: str):
+        """
+        Returns all the frame data for one column.
+
+        Parameters
+        ----------
+        name : str
+            column name.
+
+        Returns
+        -------
+        pd.DataFrame
+            one column from the dataframe.
+
+        """
         self.__check_param(name)
         return np.array(self.imu_data[str(name)])
-    
-    def set_frame(self, frame:int):
+
+    def set_frame(self, frame: int):
+        """
+        manually set the frame,
+
+        Parameters
+        ----------
+        frame : int
+            frame number.
+
+        Returns
+        -------
+        None.
+
+        """
         if frame > self.num_frames or frame < 0:
             sys.exit("Frame has to be a positive intiger, and has to be less than the total number of available frames. Number of frames: " + str(self.num_frames))
         self.frame = frame
-    
+
     def __check_param(self, name):
-        assert name in self.params_avail, "Parameter is not available in IMU. Parameters: " + ", ".join(self.params_avail)
+        """
+        Ensures the requested name is available in the DataFrame
+
+        Parameters
+        ----------
+        name : str
+            requested name.
+
+        Returns
+        -------
+        None.
+
+        """
+        assert name in self.params_avail, "Parameter is not available in IMU. Parameters: " + \
+            ", ".join(self.params_avail)
