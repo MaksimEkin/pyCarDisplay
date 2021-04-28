5e50582a59b00e39cd39aaa02973652411c82aee
ryancb4 <ryancb4@umbc.edu>
2021-04-27 23:43:33 -0400

    documentation added

--- pyCarDisplay/sensors/kalman_filter_api.py
+++ pyCarDisplay/sensors/kalman_filter_api.py
@@ -1,12 +1,22 @@
 """
-
+Kalman filter definition to smooth out noise in sensor readings
 """
 import numpy as np
 class KalmanFilter(object):
-    """
-
-    """
     def __init__(self, P = 1, H = 1, F = 1, Q = 0.1):
+        """
+
+        Parameters
+        ----------
+        P : covariance of the observation noise
+        H : observation model
+        F : state-transition model
+        Q : covariance of the process noise
+
+        Returns
+        -------
+
+        """
         self.P = P
         self.H = H
         self.F = F
@@ -17,13 +27,13 @@ class KalmanFilter(object):
 
         Parameters
         ----------
-        acceleration :
-        speed_previous :
-        delta_time :
+        acceleration : new data reading
+        speed_previous : previous data point
+        delta_time : change in time from last sensor reading
 
         Returns
         -------
-
+        speed_predict : predicted data output
         """
         # Prediction Model: speed = F * previous speed + acceleration * delata_time
         speed_predict = self.F * speed_previous + acceleration * delta_time
@@ -38,13 +48,13 @@ class KalmanFilter(object):
 
         Parameters
         ----------
-        speed_data_from_sensor :
-        R :
-        speed_predict :
+        speed_data_from_sensor : sensor data read
+        R : covariance
+        speed_predict : predicted datapoint
 
         Returns
         -------
-
+        speed_fused : data point thathas been filtered
         """
         y = speed_data_from_sensor - self.H * speed_predict
         S = self.H * self.P * self.H + R
